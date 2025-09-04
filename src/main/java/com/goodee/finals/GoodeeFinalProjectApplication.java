57c0aec7a93d5ec0bd4181ab25906fcaf2deb825
Coma <comasocean@gmail.com>
2025-09-04 19:24:50 +0900

    Create: Spring Maven Project 생성 + 의존성 추가

---
+++ src/main/java/com/goodee/finals/GoodeeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.finals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodeeFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodeeFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodee/finals/GoodeeFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.finals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodeeFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
