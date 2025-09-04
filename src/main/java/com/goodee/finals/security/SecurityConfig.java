7dc683c51efda57fad368651e10ae953944ffac8
Coma <comasocean@gmail.com>
2025-09-04 20:01:39 +0900

    Create: Index + Security 초기 설정

---
+++ src/main/java/com/goodee/finals/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.goodee.finals.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+		security
+		
+		// Authorization
+		.authorizeHttpRequests(auth -> {
+			auth
+					// static resources
+					.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+					// 임시
+					.anyRequest().permitAll()
+					;
+		})
+		
+		;
+		
+		return security.build();
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+</head>
+
+<body style="height: 100vh; background-color: black; display: flex; justify-content: center; align-items: center;">
+	<div>
+		<iframe width="1120" height="630" src="https://www.youtube.com/embed/mMjomLgUN_8?si=RYGl9qw_Za_Ynci9&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+	</div>
+</body>
+
+</html>
