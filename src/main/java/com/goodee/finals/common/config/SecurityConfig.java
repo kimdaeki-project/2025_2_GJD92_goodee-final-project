d7fa3aebe4dd2c5516d1beef93bc1af37de89f97
Coma <comasocean@gmail.com>
2025-09-05 14:37:08 +0900

    Update: 패키지명 변경

--- src/main/java/com/goodee/finals/common/config/SecurityConfig.java
+++ src/main/java/com/goodee/finals/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.goodee.finals.security;
+package com.goodee.finals.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
