7ff9400c3a48015af82d4a56fd0bcd639069747b
Coma <comasocean@gmail.com>
2025-09-11 10:43:40 +0900

    Create: 로그인, 로그아웃 구현

---
+++ src/main/java/com/goodee/finals/common/security/CustomLogoutSuccessHandler.java
@@ -0,0 +1,25 @@
+package com.goodee.finals.common.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+		log.info("로그아웃 성공!");
+		
+		response.sendRedirect("/");
+	}
+
+}
--- src/main/java/com/goodee/finals/staff/StaffService.java
+++ src/main/java/com/goodee/finals/staff/StaffService.java
@@ -1,5 +1,6 @@
 package com.goodee.finals.staff;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,10 +8,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StaffService implements UserDetailsService {
+	@Autowired
+	private StaffRepository staffRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		Integer staffCode;
+		
+		try {
+			staffCode = Integer.parseInt(username);
+		} catch (NumberFormatException e) {
+			throw new UsernameNotFoundException("올바른 사원번호를 입력해주세요.");
+		}
+		
+		StaffDTO staffDTO = staffRepository.findById(staffCode).orElseThrow();
+		
+		return staffDTO;
 	}
 
 }
