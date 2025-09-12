b379220589a05ec87f96272cac8e5d6d8f7528ac
Coma <comasocean@gmail.com>
2025-09-12 10:59:21 +0900

    Update: 사원, 부서, 직급 엔티티 구조 수정

--- src/test/java/com/goodee/finals/staff/StaffRepositoryTest.java
+++ src/test/java/com/goodee/finals/staff/StaffRepositoryTest.java
@@ -10,49 +10,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootTest
 class StaffRepositoryTest {
 	@Autowired
+	private StaffRepository staffRepository;
+	@Autowired
 	private DeptRepository deptRepository;
 	@Autowired
 	private JobRepository jobRepository;
 	@Autowired
-	private StaffRepository staffRepository;
-	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	@Test
-	void deptInsertTest() {
-		DeptDTO deptDTO = new DeptDTO();
-		deptDTO.setDeptCode(1000);
-		deptDTO.setDeptName("인사");
-		
-		deptRepository.saveAndFlush(deptDTO);
-	}
 	
 	@Test
 	void staffInsertTest() {
 		StaffDTO staffDTO = new StaffDTO();
 		staffDTO.setStaffCode(20250001);
 		staffDTO.setStaffPw(passwordEncoder.encode("0000"));
-		
-		StaffDeptDTO staffDeptDTO = new StaffDeptDTO();
-		DeptDTO deptDTO = deptRepository.findById(1000).get();
-		staffDeptDTO.setDeptDTO(deptDTO);
-		staffDeptDTO.setStaffDTO(staffDTO);
-		staffDTO.setStaffDeptDTO(staffDeptDTO);
-		
-		StaffJobDTO staffJobDTO = new StaffJobDTO();
-		JobDTO jobDTO = jobRepository.findById(1100).get();
-		staffJobDTO.setJobDTO(jobDTO);
-		staffJobDTO.setStaffDTO(staffDTO);
-		staffDTO.setStaffJobDTO(staffJobDTO);
+		staffDTO.setDeptDTO(deptRepository.findById(1000).orElseThrow());
+		staffDTO.setJobDTO(jobRepository.findById(1100).orElseThrow());
 		
 		staffRepository.saveAndFlush(staffDTO);
-		
 	}
 	
 	@Test
 	void staffSelectTest() {
-		StaffDTO staffDTO = staffRepository.findById(20250001).get();
-		System.out.println(staffDTO.getStaffJobDTO().getJobDTO().getJobName());
+		StaffDTO staffDTO = staffRepository.findById(20250001).orElseThrow();
+		System.out.println(staffDTO.getJobDTO().getJobName());
 	}
 
 }
