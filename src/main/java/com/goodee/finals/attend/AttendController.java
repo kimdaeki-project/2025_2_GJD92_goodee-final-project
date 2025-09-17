e84a747ba412be9e93b642ae28fe7f69b0b216b5
kimhyewon92 <sing92@naver.com>
2025-09-17 11:19:19 +0900

    Update: 출퇴근 버튼 method post로 변경

--- src/main/java/com/goodee/finals/attend/AttendController.java
+++ src/main/java/com/goodee/finals/attend/AttendController.java
@@ -4,37 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/attend/**")
+@Slf4j
 public class AttendController {
 
 	@Autowired
 	private AttendService attendService;
 	
-	@GetMapping("in")
+	@PostMapping("in")
 	public String attendIn(AttendDTO attendDTO, Model model) {
-		
 		AttendDTO result = attendService.attendIn(attendDTO);
 		
 		if (result != null) {
 			model.addAttribute("attendDTO", result);
-			System.out.println("출근체크");
-			return "index";
+			log.info("출근체크");
+			return "redirect:/";
 		} else {
 			return null;
 		}
 		
 	}
 	
-	@GetMapping("out")
+	@PostMapping("out")
 	public String attendOut(AttendDTO attendDTO, Model model) {
 		AttendDTO result = attendService.attendOut(attendDTO);
 		
 		if (result != null) {
 			model.addAttribute("attendDTO", result);
-			System.out.println("퇴근체크");
+			log.info("퇴근체크");
 			return "redirect:/";
 		} else {
 			return null;
