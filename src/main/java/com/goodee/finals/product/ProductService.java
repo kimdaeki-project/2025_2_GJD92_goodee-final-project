af9613c5bdf14b0222adf9ca81fa94113a90e77c
Coma <comasocean@gmail.com>
2025-09-30 20:21:01 +0900

    Fix: 발견된 오류 수정

--- src/main/java/com/goodee/finals/product/ProductService.java
+++ src/main/java/com/goodee/finals/product/ProductService.java
@@ -1,21 +1,18 @@
 package com.goodee.finals.product;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.goodee.finals.common.attachment.AttachmentDTO;
 import com.goodee.finals.common.attachment.AttachmentRepository;
 import com.goodee.finals.common.attachment.ProductAttachmentDTO;
 import com.goodee.finals.common.file.FileService;
-import com.goodee.finals.productManage.ProductManageRepository;
 import com.goodee.finals.staff.StaffDTO;
 import com.goodee.finals.staff.StaffRepository;
 
@@ -121,7 +118,8 @@ public class ProductService {
 			boolean deleteResult = fileService.fileDelete(FileService.PRODUCT, savedName);
 			
 			if(deleteResult) {
-				attachmentRepository.deleteById(beforeAttach.getAttachNum());
+				productRepository.deleteBeforeAttach(beforeAttach.getAttachNum());
+				attachmentRepository.deleteAttach(beforeAttach.getAttachNum());
 			}
 			
 			AttachmentDTO attachmentDTO = new AttachmentDTO();
