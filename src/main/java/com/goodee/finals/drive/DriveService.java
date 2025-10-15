c83ccea793c44eadb8e57b4fb6408c9deb3461bc
RealNixC-C <john7622@naver.com>
2025-10-15 18:06:14 +0900

    Add : 사원 비활성화, 활성화 시 부서 드라이브에서 추가, 제거

--- src/main/java/com/goodee/finals/drive/DriveService.java
+++ src/main/java/com/goodee/finals/drive/DriveService.java
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.goodee.finals.common.attachment.AttachmentDTO;
 import com.goodee.finals.common.attachment.AttachmentRepository;
 import com.goodee.finals.common.file.FileService;
+import com.goodee.finals.ride.RideRepository;
 import com.goodee.finals.staff.DeptDTO;
 import com.goodee.finals.staff.DeptRepository;
 import com.goodee.finals.staff.JobDTO;
@@ -32,6 +33,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Transactional(rollbackFor = Exception.class)
 public class DriveService {
+
+    private final RideRepository rideRepository;
 	private static final Long ROLE_HQ_DRIVE = 2L;
 	private static final Long ROLE_HR_DRIVE = 3L;
 	private static final Long ROLE_OP_DRIVE = 4L;
@@ -56,6 +59,10 @@ public class DriveService {
 	private DriveShareRepository driveShareRepository;
 	@Autowired
 	private FileService fileService;
+
+    DriveService(RideRepository rideRepository) {
+        this.rideRepository = rideRepository;
+    }
 
 	public List<DeptDTO> getDeptList() {
 		return deptRepository.findAll();
@@ -132,29 +139,42 @@ public class DriveService {
 	}
 
 	// 사원 등록시 부서 공유드라이브에 추가
-	public DriveDTO addInDeptDrive(StaffDTO staffDTO) {
-		Integer deptCode = staffDTO.getDeptDTO().getDeptCode();
-		DriveDTO driveDTO = null;
+	public boolean addInDeptDrive(StaffDTO staffDTO) {
+		DriveDTO driveDTO = findDeptDrive(staffDTO);
 		
-		switch (deptCode) {
-			case 1000: driveDTO = driveRepository.findById(ROLE_HQ_DRIVE).orElseThrow(); break;
-			case 1001: driveDTO = driveRepository.findById(ROLE_HR_DRIVE).orElseThrow(); break;
-			case 1002: driveDTO = driveRepository.findById(ROLE_OP_DRIVE).orElseThrow(); break;
-			case 1003: driveDTO = driveRepository.findById(ROLE_FA_DRIVE).orElseThrow(); break;
+		List<DriveShareDTO> dsList = driveDTO.getDriveShareDTOs();
+		if(dsList == null) { // || isEmpty() 조건 추가시 영속성 추적 불가 에러 발생
+			dsList = new ArrayList<DriveShareDTO>();
 		}
+		int checkSize = dsList.size();
 		
-		if(driveDTO.getDriveShareDTOs() == null) { // || isEmpty() 조건 추가시 영속성 추적 불가 에러 발생
-			driveDTO.setDriveShareDTOs(new ArrayList<DriveShareDTO>());
-		} 	
 		DriveShareDTO driveShareDTO = new DriveShareDTO();
 		driveShareDTO.setDriveDTO(driveDTO);
 		driveShareDTO.setStaffDTO(staffDTO);
+		dsList.add(driveShareDTO);
 		driveDTO.setIsPersonal(false);
-		driveDTO.getDriveShareDTOs().add(driveShareDTO);
+		driveDTO.setDriveShareDTOs(dsList);
 		
 		driveDTO = driveRepository.save(driveDTO);
+		if(driveDTO.getDriveShareDTOs().size() > checkSize) return true;
+		else return false;
+	}
+	
+	public boolean deleteFromDeptDrive(StaffDTO staffDTO) {
+		DriveDTO driveDTO = findDeptDrive(staffDTO);
+		Integer disabledStaffCode = staffDTO.getStaffCode();
 		
-		return driveDTO;
+		List<DriveShareDTO> dsList = driveDTO.getDriveShareDTOs();
+		for(int i = 0; i < dsList.size(); i++) {
+			Integer dsStaffCode = dsList.get(i).getStaffDTO().getStaffCode();
+			if(disabledStaffCode.equals(dsStaffCode)) {
+				dsList.remove(i);
+				driveDTO.setDriveShareDTOs(dsList);
+				driveRepository.saveAndFlush(driveDTO);
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public DriveDTO createDrive(DriveDTO driveDTO) {
@@ -313,5 +333,18 @@ public class DriveService {
 		return attachmentRepository.findAllById(attachNums);
 	}
 	
+	public DriveDTO findDeptDrive(StaffDTO staffDTO) {
+		Integer deptCode = staffDTO.getDeptDTO().getDeptCode();
+		DriveDTO driveDTO = null;	
+		
+		switch (deptCode) {
+			case 1000: driveDTO = driveRepository.findById(ROLE_HQ_DRIVE).orElseThrow(); break;
+			case 1001: driveDTO = driveRepository.findById(ROLE_HR_DRIVE).orElseThrow(); break;
+			case 1002: driveDTO = driveRepository.findById(ROLE_OP_DRIVE).orElseThrow(); break;
+			case 1003: driveDTO = driveRepository.findById(ROLE_FA_DRIVE).orElseThrow(); break;
+		}
+		
+		return driveDTO;
+	}
 	
 }
